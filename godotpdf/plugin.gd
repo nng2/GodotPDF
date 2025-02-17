ed77da76e8ac583d6e0af019edc4ec729f120df8
nng2 <114796199+nng2@users.noreply.github.com>
2025-02-17 08:14:21 -0800

    Delete godotpdf directory

--- godotpdf/plugin.gd
+++
@@ -1,9 +0,0 @@
-@tool
-extends EditorPlugin
-
-const NAME = "PDF"
-func _enable_plugin() -> void:
-	add_autoload_singleton(NAME, "res://addons/godotpdf/PDF.gd")
-
-func _disable_plugin() -> void:
-	remove_autoload_singleton(NAME)
